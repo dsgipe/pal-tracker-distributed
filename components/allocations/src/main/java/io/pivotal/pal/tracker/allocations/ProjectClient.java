46ead0a93d5808b38d9f13c11b3bb92f7f0a6c7a
pal_user <dsgipe@gmail.com>
2018-07-16 13:54:41 -0600

    refactored with redis

--- components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
+++ components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
@@ -1,21 +1,31 @@
 package io.pivotal.pal.tracker.allocations;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 import org.springframework.web.client.RestOperations;
 
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 public class ProjectClient {
     private Logger logger;
-    private final Map<Long, ProjectInfo> projectsCache = new ConcurrentHashMap<>();
 
     private final RestOperations restOperations;
     private final String registrationServerEndpoint;
 
+    private ConcurrentMap<Long, ProjectInfo> map;
+    private RedissonClient redisson;
+    private void ConfigureRedis(){
+        redisson = Redisson.create();
+        map=redisson.getMap("someMap");
+        }
+
     public ProjectClient(RestOperations restOperations, String registrationServerEndpoint) {
+
+        ConfigureRedis();
+
         logger = LoggerFactory.getLogger(getClass());
         this.restOperations= restOperations;
         this.registrationServerEndpoint = registrationServerEndpoint;
@@ -25,13 +35,14 @@ public class ProjectClient {
     public ProjectInfo getProject(long projectId) {
         ProjectInfo projectInfo = restOperations.getForObject
                 (registrationServerEndpoint + "/projects/" + projectId, ProjectInfo.class);
-        projectsCache.put(projectId,projectInfo);
+
+        map.put(projectId,projectInfo);
         return projectInfo;
     }
 
     public ProjectInfo getProjectFromCache(long projectId) {
         logger.info("Getting project with id {} from cache", projectId);
-        return projectsCache.get(projectId);
+        return map.get(projectId);
     }
 
 }
