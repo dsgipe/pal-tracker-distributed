69612b61ae6784c76364e04462cf4e97ed8126f2
pal_user <dsgipe@gmail.com>
2018-07-17 10:28:32 -0600

    removed some redis stuff

--- components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
+++ components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
@@ -1,38 +1,21 @@
 package io.pivotal.pal.tracker.allocations;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-import org.redisson.Redisson;
-import org.redisson.api.RedissonClient;
-import org.redisson.config.Config;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 import org.springframework.web.client.RestOperations;
 
-import java.util.concurrent.ConcurrentMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ProjectClient {
     private Logger logger;
+    private final Map<Long, ProjectInfo> projectsCache = new ConcurrentHashMap<>();
 
     private final RestOperations restOperations;
     private final String registrationServerEndpoint;
 
-    private ConcurrentMap<Long, ProjectInfo> map;
-
-    private RedissonClient redisson;
-    private void ConfigureRedis(){
-        //GetRunningEnvironmentVariablesRequest.build();
-        String host = "10.0.8.3";
-        int port = 43191;
-        Config config = new Config();
-        config.useSingleServer().setAddress("redis://"+host +":" + port);
-        redisson = Redisson.create();
-        map=redisson.getMap("someMap");
-        }
-
     public ProjectClient(RestOperations restOperations, String registrationServerEndpoint) {
-
-        ConfigureRedis();
-
         logger = LoggerFactory.getLogger(getClass());
         this.restOperations= restOperations;
         this.registrationServerEndpoint = registrationServerEndpoint;
@@ -42,14 +25,13 @@ public class ProjectClient {
     public ProjectInfo getProject(long projectId) {
         ProjectInfo projectInfo = restOperations.getForObject
                 (registrationServerEndpoint + "/projects/" + projectId, ProjectInfo.class);
-
-        map.put(projectId,projectInfo);
+        projectsCache.put(projectId,projectInfo);
         return projectInfo;
     }
 
     public ProjectInfo getProjectFromCache(long projectId) {
         logger.info("Getting project with id {} from cache", projectId);
-        return map.get(projectId);
+        return projectsCache.get(projectId);
     }
 
 }
